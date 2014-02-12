b52e1faa9b23c9a166454eab0f91474d8077edfe
Andre Martins <alm253@cornell.edu>
2014-02-12 16:54:21 -0500

    Indent test code

--- bigWig/inst/tests/test_boundary.R
+++ bigWig/inst/tests/test_boundary.R
@@ -2,102 +2,102 @@ context("boundary tests")
 
 test_that("region probe sum", {
 
-# start/end within probe
-# chrP 60 65 9
-res = region.probeQuery.bigWig(bwTest, "chrP", 61, 63, op = "sum")
-expect_equal(res, 9)
+  # start/end within probe
+  # chrP 60 65 9
+  res = region.probeQuery.bigWig(bwTest, "chrP", 61, 63, op = "sum")
+  expect_equal(res, 9)
 
-# start/end covers probe
-res = region.probeQuery.bigWig(bwTest, "chrP", 58, 66, op = "sum")
-expect_equal(res, 9)
+  # start/end covers probe
+  res = region.probeQuery.bigWig(bwTest, "chrP", 58, 66, op = "sum")
+  expect_equal(res, 9)
 
-# start @ edge of probe / end inside
-# chrP    10  20    10
-# chrP    20  30     5
-res = region.probeQuery.bigWig(bwTest, "chrP", 20, 25, op = "sum")
-expect_equal(res, 5)
+  # start @ edge of probe / end inside
+  # chrP    10  20    10
+  # chrP    20  30     5
+  res = region.probeQuery.bigWig(bwTest, "chrP", 20, 25, op = "sum")
+  expect_equal(res, 5)
 
-# start inside / end @ edge of probe
-res = region.probeQuery.bigWig(bwTest, "chrP", 15, 20, op = "sum")
-expect_equal(res, 10)
+  # start inside / end @ edge of probe
+  res = region.probeQuery.bigWig(bwTest, "chrP", 15, 20, op = "sum")
+  expect_equal(res, 10)
 
-# start inside / end over next probe
-res = region.probeQuery.bigWig(bwTest, "chrP", 15, 21, op = "sum")
-expect_equal(res, 15)
+  # start inside / end over next probe
+  res = region.probeQuery.bigWig(bwTest, "chrP", 15, 21, op = "sum")
+  expect_equal(res, 15)
 
-# end after end of chrom
-# chrP 100
-# chrP    60  65     9
-res = region.probeQuery.bigWig(bwTest, "chrP", 60, 120, op = "sum")
-expect_equal(res, 9)
+  # end after end of chrom
+  # chrP 100
+  # chrP    60  65     9
+  res = region.probeQuery.bigWig(bwTest, "chrP", 60, 120, op = "sum")
+  expect_equal(res, 9)
 
-# covers nothing (gap.value)
-res = region.probeQuery.bigWig(bwTest, "chrP", 70, 120, op = "sum")
-expect_true(is.na(res))
+  # covers nothing (gap.value)
+  res = region.probeQuery.bigWig(bwTest, "chrP", 70, 120, op = "sum")
+  expect_true(is.na(res))
 
-# edges: start < 0
-expect_error(region.probeQuery.bigWig(bwTest, "chrP", -2, 10, op="sum"))
+  # edges: start < 0
+  expect_error(region.probeQuery.bigWig(bwTest, "chrP", -2, 10, op="sum"))
 
-# end > end of chrom
-# explicit gap value
-res = region.probeQuery.bigWig(bwTest, "chrP", 70, 120, op = "sum", gap.value = 2)
-expect_equal(res, 2)
+  # end > end of chrom
+  # explicit gap value
+  res = region.probeQuery.bigWig(bwTest, "chrP", 70, 120, op = "sum", gap.value = 2)
+  expect_equal(res, 2)
 
-# end <= start
-expect_error(region.probeQuery.bigWig(bwTest, "chrP", 1, 1, op = "sum"))
-expect_error(region.probeQuery.bigWig(bwTest, "chrP", 3, 1, op = "sum"))
+  # end <= start
+  expect_error(region.probeQuery.bigWig(bwTest, "chrP", 1, 1, op = "sum"))
+  expect_error(region.probeQuery.bigWig(bwTest, "chrP", 3, 1, op = "sum"))
 })
 
 
 test_that("region base pair sum", {
 
-# start/end within probe
-# chrP 60 65 9
-res = region.bpQuery.bigWig(bwTest, "chrP", 61, 63, op = "sum")
-expect_equal(res, 9*(63 - 61))
+  # start/end within probe
+  # chrP 60 65 9
+  res = region.bpQuery.bigWig(bwTest, "chrP", 61, 63, op = "sum")
+  expect_equal(res, 9*(63 - 61))
 
-# start/end covers probe
-res = region.bpQuery.bigWig(bwTest, "chrP", 58, 66, op = "sum")
-expect_equal(res, 9*(65 - 60))
+  # start/end covers probe
+  res = region.bpQuery.bigWig(bwTest, "chrP", 58, 66, op = "sum")
+  expect_equal(res, 9*(65 - 60))
 
-# start @ edge of probe / end inside
-# chrP    10  20    10
-# chrP    20  30     5
-res = region.bpQuery.bigWig(bwTest, "chrP", 20, 25, op = "sum")
-expect_equal(res, 5*(25 - 20))
+  # start @ edge of probe / end inside
+  # chrP    10  20    10
+  # chrP    20  30     5
+  res = region.bpQuery.bigWig(bwTest, "chrP", 20, 25, op = "sum")
+  expect_equal(res, 5*(25 - 20))
 
-# chrB 1 2 1
-# chrB 2 3 2
-res = region.bpQuery.bigWig(bwTest, "chrB", 2, 3, op = "sum")
-expect_equal(res, 2)
+  # chrB 1 2 1
+  # chrB 2 3 2
+  res = region.bpQuery.bigWig(bwTest, "chrB", 2, 3, op = "sum")
+  expect_equal(res, 2)
 
-# start inside / end @ edge of probe
-res = region.bpQuery.bigWig(bwTest, "chrP", 15, 20, op = "sum")
-expect_equal(res, 10 * (20 - 15))
+  # start inside / end @ edge of probe
+  res = region.bpQuery.bigWig(bwTest, "chrP", 15, 20, op = "sum")
+  expect_equal(res, 10 * (20 - 15))
 
-# start inside / end over next probe
-res = region.bpQuery.bigWig(bwTest, "chrP", 15, 21, op = "sum")
-expect_equal(res, 10*(20 - 15) + 5)
+  # start inside / end over next probe
+  res = region.bpQuery.bigWig(bwTest, "chrP", 15, 21, op = "sum")
+  expect_equal(res, 10*(20 - 15) + 5)
 
-# end after end of chrom
-# chrP 100
-# chrP    60  65     9
-res = region.bpQuery.bigWig(bwTest, "chrP", 60, 120, op = "sum")
-expect_equal(res, 9*(65 - 60))
+  # end after end of chrom
+  # chrP 100
+  # chrP    60  65     9
+  res = region.bpQuery.bigWig(bwTest, "chrP", 60, 120, op = "sum")
+  expect_equal(res, 9*(65 - 60))
 
-# covers nothing (gap.value)
-res = region.bpQuery.bigWig(bwTest, "chrP", 70, 120, op = "sum")
-expect_equal(res, 0) # default gap.value
+  # covers nothing (gap.value)
+  res = region.bpQuery.bigWig(bwTest, "chrP", 70, 120, op = "sum")
+  expect_equal(res, 0) # default gap.value
 
-# edges: start < 0
-expect_error(region.bpQuery.bigWig(bwTest, "chrP", -2, 10, op="sum"))
+  # edges: start < 0
+  expect_error(region.bpQuery.bigWig(bwTest, "chrP", -2, 10, op="sum"))
 
-# end > end of chrom
-# explicit gap value
-res = region.bpQuery.bigWig(bwTest, "chrP", 70, 120, op = "sum", gap.value = 2)
-expect_equal(res, 2)
+  # end > end of chrom
+  # explicit gap value
+  res = region.bpQuery.bigWig(bwTest, "chrP", 70, 120, op = "sum", gap.value = 2)
+  expect_equal(res, 2)
 
-# end <= start
-expect_error(region.bpQuery.bigWig(bwTest, "chrP", 1, 1, op = "sum"))
-expect_error(region.bpQuery.bigWig(bwTest, "chrP", 3, 1, op = "sum"))
+  # end <= start
+  expect_error(region.bpQuery.bigWig(bwTest, "chrP", 1, 1, op = "sum"))
+  expect_error(region.bpQuery.bigWig(bwTest, "chrP", 3, 1, op = "sum"))
 })
--- bigWig/inst/tests/test_query_mods.R
+++ bigWig/inst/tests/test_query_mods.R
@@ -2,24 +2,25 @@ context("query modifiers")
 
 test_that("step >= 1", {
 
-bed = data.frame("chrB", 10, 20)
-bed6 = data.frame("chrB", 10, 20, "N", 0, "+")
+  bed = data.frame("chrB", 10, 20)
+  bed6 = data.frame("chrB", 10, 20, "N", 0, "+")
 
-for (step in c(-1, 0)) {
-  expect_error(step.bpQuery.bigWig(bwTest, "chrB", 10, 20, step))
-  expect_error(step.bpQuery.bigWig(bwTest, "chrB", NULL, NULL, step))
-  expect_error(bed.step.bpQuery.bigWig(bwTest, bed, step))
-  expect_error(bed6.step.bpQuery.bigWig(bwTest, bwTest, bed6, step))
+  for (step in c(-1, 0)) {
+    expect_error(step.bpQuery.bigWig(bwTest, "chrB", 10, 20, step))
+    expect_error(step.bpQuery.bigWig(bwTest, "chrB", NULL, NULL, step))
+    expect_error(bed.step.bpQuery.bigWig(bwTest, bed, step))
+    expect_error(bed6.step.bpQuery.bigWig(bwTest, bwTest, bed6, step))
 
-  expect_error(step.probeQuery.bigWig(bwTest, "chrB", 10, 20, step))
-  expect_error(step.probeQuery.bigWig(bwTest, "chrB", NULL, NULL, step))
-  expect_error(bed.step.probeQuery.bigWig(bwTest, bed, step))
-  expect_error(bed6.step.probeQuery.bigWig(bwTest, bwTest, bed6, step))
-}
+    expect_error(step.probeQuery.bigWig(bwTest, "chrB", 10, 20, step))
+    expect_error(step.probeQuery.bigWig(bwTest, "chrB", NULL, NULL, step))
+    expect_error(bed.step.probeQuery.bigWig(bwTest, bed, step))
+    expect_error(bed6.step.probeQuery.bigWig(bwTest, bwTest, bed6, step))
+  }
 })
 
 test_that("query not multiple of step size", {
 
+  
 # TODO: check for warning
 
 
@@ -29,46 +30,46 @@ test_that("query not multiple of step size", {
 
 test_that("abs.value", {
 
-bed = data.frame(c("chrB", "chrBm"), 10, 20)
-bed6 = data.frame(c("chrB", "chrBm"), 10, 20, "N", 0, "+")
+  bed = data.frame(c("chrB", "chrBm"), 10, 20)
+  bed6 = data.frame(c("chrB", "chrBm"), 10, 20, "N", 0, "+")
 
-expect_true(region.bpQuery.bigWig(bwTest, "chrBm", 10, 20) < 0)
-expect_true(region.bpQuery.bigWig(bwTest, "chrBm", 10, 20, abs.value = TRUE) >= 0)
+  expect_true(region.bpQuery.bigWig(bwTest, "chrBm", 10, 20) < 0)
+  expect_true(region.bpQuery.bigWig(bwTest, "chrBm", 10, 20, abs.value = TRUE) >= 0)
 
-expect_equal(bed.region.bpQuery.bigWig(bwTest, bed) < 0,  c(FALSE, TRUE))
-expect_equal(bed.region.bpQuery.bigWig(bwTest, bed, abs.value = TRUE) < 0, c(FALSE, FALSE))
+  expect_equal(bed.region.bpQuery.bigWig(bwTest, bed) < 0,  c(FALSE, TRUE))
+  expect_equal(bed.region.bpQuery.bigWig(bwTest, bed, abs.value = TRUE) < 0, c(FALSE, FALSE))
 
-expect_equal(bed6.region.bpQuery.bigWig(bwTest, bwTest, bed6) < 0, c(FALSE, TRUE))
-expect_equal(bed6.region.bpQuery.bigWig(bwTest, bwTest, bed6, abs.value = TRUE) < 0, c(FALSE, FALSE))
+  expect_equal(bed6.region.bpQuery.bigWig(bwTest, bwTest, bed6) < 0, c(FALSE, TRUE))
+  expect_equal(bed6.region.bpQuery.bigWig(bwTest, bwTest, bed6, abs.value = TRUE) < 0, c(FALSE, FALSE))
 
-expect_true(all(step.bpQuery.bigWig(bwTest, "chrBm", 10, 20, 1) <= 0))
-expect_true(all(step.bpQuery.bigWig(bwTest, "chrBm", 10, 20, 1, abs.value = TRUE) >= 0))
+  expect_true(all(step.bpQuery.bigWig(bwTest, "chrBm", 10, 20, 1) <= 0))
+  expect_true(all(step.bpQuery.bigWig(bwTest, "chrBm", 10, 20, 1, abs.value = TRUE) >= 0))
 
-expect_true(all(bed.step.bpQuery.bigWig(bwTest, bed, 1, as.matrix = TRUE)[2,] <= 0))
-expect_true(all(bed.step.bpQuery.bigWig(bwTest, bed, 1, as.matrix = TRUE, abs.value = TRUE) >= 0))
+  expect_true(all(bed.step.bpQuery.bigWig(bwTest, bed, 1, as.matrix = TRUE)[2,] <= 0))
+  expect_true(all(bed.step.bpQuery.bigWig(bwTest, bed, 1, as.matrix = TRUE, abs.value = TRUE) >= 0))
 
-expect_true(all(bed6.step.bpQuery.bigWig(bwTest, bwTest, bed6, 1, as.matrix = TRUE)[2,] <= 0))
-expect_true(all(bed6.step.bpQuery.bigWig(bwTest, bwTest, bed6, 1, as.matrix = TRUE, abs.value = TRUE) >= 0))
+  expect_true(all(bed6.step.bpQuery.bigWig(bwTest, bwTest, bed6, 1, as.matrix = TRUE)[2,] <= 0))
+  expect_true(all(bed6.step.bpQuery.bigWig(bwTest, bwTest, bed6, 1, as.matrix = TRUE, abs.value = TRUE) >= 0))
 
-#
-# now with probe functions
-expect_true(region.probeQuery.bigWig(bwTest, "chrBm", 10, 20) < 0)
-expect_true(region.probeQuery.bigWig(bwTest, "chrBm", 10, 20, abs.value = TRUE) >= 0)
+  #
+  # now with probe functions
+  expect_true(region.probeQuery.bigWig(bwTest, "chrBm", 10, 20) < 0)
+  expect_true(region.probeQuery.bigWig(bwTest, "chrBm", 10, 20, abs.value = TRUE) >= 0)
 
-expect_equal(bed.region.probeQuery.bigWig(bwTest, bed) < 0,  c(FALSE, TRUE))
-expect_equal(bed.region.probeQuery.bigWig(bwTest, bed, abs.value = TRUE) < 0, c(FALSE, FALSE))
+  expect_equal(bed.region.probeQuery.bigWig(bwTest, bed) < 0,  c(FALSE, TRUE))
+  expect_equal(bed.region.probeQuery.bigWig(bwTest, bed, abs.value = TRUE) < 0, c(FALSE, FALSE))
 
-expect_equal(bed6.region.probeQuery.bigWig(bwTest, bwTest, bed6) < 0, c(FALSE, TRUE))
-expect_equal(bed6.region.probeQuery.bigWig(bwTest, bwTest, bed6, abs.value = TRUE) < 0, c(FALSE, FALSE))
+  expect_equal(bed6.region.probeQuery.bigWig(bwTest, bwTest, bed6) < 0, c(FALSE, TRUE))
+  expect_equal(bed6.region.probeQuery.bigWig(bwTest, bwTest, bed6, abs.value = TRUE) < 0, c(FALSE, FALSE))
 
-expect_true(all(step.probeQuery.bigWig(bwTest, "chrBm", 10, 20, 1) < 0, na.rm = TRUE))
-expect_true(all(step.probeQuery.bigWig(bwTest, "chrBm", 10, 20, 1, abs.value = TRUE) > 0, na.rm = TRUE))
+  expect_true(all(step.probeQuery.bigWig(bwTest, "chrBm", 10, 20, 1) < 0, na.rm = TRUE))
+  expect_true(all(step.probeQuery.bigWig(bwTest, "chrBm", 10, 20, 1, abs.value = TRUE) > 0, na.rm = TRUE))
 
-expect_true(all(bed.step.probeQuery.bigWig(bwTest, bed, 1, as.matrix = TRUE)[2,] < 0, na.rm = TRUE))
-expect_true(all(bed.step.probeQuery.bigWig(bwTest, bed, 1, as.matrix = TRUE, abs.value = TRUE) > 0, na.rm = TRUE))
+  expect_true(all(bed.step.probeQuery.bigWig(bwTest, bed, 1, as.matrix = TRUE)[2,] < 0, na.rm = TRUE))
+  expect_true(all(bed.step.probeQuery.bigWig(bwTest, bed, 1, as.matrix = TRUE, abs.value = TRUE) > 0, na.rm = TRUE))
 
-expect_true(all(bed6.step.probeQuery.bigWig(bwTest, bwTest, bed6, 1, as.matrix = TRUE)[2,] < 0, na.rm = TRUE))
-expect_true(all(bed6.step.probeQuery.bigWig(bwTest, bwTest, bed6, 1, as.matrix = TRUE, abs.value = TRUE) > 0, na.rm = TRUE))
+  expect_true(all(bed6.step.probeQuery.bigWig(bwTest, bwTest, bed6, 1, as.matrix = TRUE)[2,] < 0, na.rm = TRUE))
+  expect_true(all(bed6.step.probeQuery.bigWig(bwTest, bwTest, bed6, 1, as.matrix = TRUE, abs.value = TRUE) > 0, na.rm = TRUE))
 })
 
 test_that("strand reverse result", {
@@ -99,22 +100,22 @@ test_that("strand reverse result", {
 })
 
 test_that("as.matrix", {
-#
-# this only applies to bed.step.* and bed6.step.* operations
+  #
+  # this only applies to bed.step.* and bed6.step.* operations
 
-bed = data.frame(c("chrB", "chrBm"), 10, 20)
-bed6 = data.frame(c("chrB", "chrBm"), 10, 20, "N", 0, "+")
+  bed = data.frame(c("chrB", "chrBm"), 10, 20)
+  bed6 = data.frame(c("chrB", "chrBm"), 10, 20, "N", 0, "+")
 
-expect_is(bed.step.bpQuery.bigWig(bwTest, bed, 1), "list")
-expect_is(bed.step.bpQuery.bigWig(bwTest, bed, 1, as.matrix = TRUE), "matrix")
+  expect_is(bed.step.bpQuery.bigWig(bwTest, bed, 1), "list")
+  expect_is(bed.step.bpQuery.bigWig(bwTest, bed, 1, as.matrix = TRUE), "matrix")
 
-expect_is(bed6.step.bpQuery.bigWig(bwTest, bwTest, bed6, 1), "list")
-expect_is(bed6.step.bpQuery.bigWig(bwTest, bwTest, bed6, 1, as.matrix = TRUE), "matrix")
+  expect_is(bed6.step.bpQuery.bigWig(bwTest, bwTest, bed6, 1), "list")
+  expect_is(bed6.step.bpQuery.bigWig(bwTest, bwTest, bed6, 1, as.matrix = TRUE), "matrix")
 
-expect_is(bed.step.probeQuery.bigWig(bwTest, bed, 1), "list")
-expect_is(bed.step.probeQuery.bigWig(bwTest, bed, 1, as.matrix = TRUE), "matrix")
+  expect_is(bed.step.probeQuery.bigWig(bwTest, bed, 1), "list")
+  expect_is(bed.step.probeQuery.bigWig(bwTest, bed, 1, as.matrix = TRUE), "matrix")
 
-expect_is(bed6.step.probeQuery.bigWig(bwTest, bwTest, bed6, 1), "list")
-expect_is(bed6.step.probeQuery.bigWig(bwTest, bwTest, bed6, 1, as.matrix = TRUE), "matrix")
+  expect_is(bed6.step.probeQuery.bigWig(bwTest, bwTest, bed6, 1), "list")
+  expect_is(bed6.step.probeQuery.bigWig(bwTest, bwTest, bed6, 1, as.matrix = TRUE), "matrix")
 })
 
