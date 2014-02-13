f010f23d92de5ff1a4cd82f8b9256867c7897215
Andre Martins <alm253@cornell.edu>
2014-02-13 17:11:55 -0500

    Data matrix scalling functions

---
+++ bigWig/R/mat.scale.R
@@ -0,0 +1,64 @@
+#
+# Functions to scale data matrices
+#
+
+# "RPKM stands for Reads Per Kilobase of transcript per Million mapped 
+#  reads. FPKM stands for Fragments Per Kilobase of transcript per Million
+#  mapped reads. "
+#
+# from
+#  http://cufflinks.cbcb.umd.edu/faq.html
+rpkm.scale <- function(mat, step, libSize) {
+  scaleFactor = (step / 1000) / (libSize / 10^6)
+  
+  mat * scaleFactor
+}
+
+# rows are scaled to sum to one
+densityToOne.scale <- function(mat, na.on.zero = TRUE) {
+  t(apply(mat, 1, function(row) {
+    if (all(is.na(row)))
+      return(row)
+    
+    total = sum(row, na.rm = TRUE)
+    if (total == 0) {
+      if (na.on.zero)
+        return(rep(NA, length(row)))
+      else
+        return(rep(0, length(row)))
+    }
+    row / total
+  }))
+}
+
+maxToOne.scale <- function(mat) {
+  t(apply(mat, 1, function(row) {
+    if (all(is.na(row)))
+      return(row)
+    
+    high = max(row, na.rm = TRUE)
+    if (high == 0)
+      return(rep(0, length(row)))
+    
+    row / high
+  }))
+}
+
+zeroToOne.scale <- function(mat) {
+  t(apply(mat, 1, function(row) {
+    if (all(is.na(row)))
+      return(row)
+    
+    high = max(row, na.rm = TRUE)
+    low = min(row, na.rm = TRUE)
+    
+    if (high == low) {
+      if (high == 0)
+        return(rep(0, length(row)))
+      else
+        return(rep(1, length(row)))
+    }
+    
+    (row - low) / (high - low)
+  }))
+}
