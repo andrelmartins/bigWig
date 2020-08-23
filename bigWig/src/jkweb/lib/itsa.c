beac5a7e2a8fcb44b5bbc126627fa686327cb093
mjg54 <guertinmj@gmail.com>
2020-08-23 15:27:49 -0400

    suppress warning
    WARNING:
    itsa.c:71:56: warning: format specifies type 'intmax_t' (aka 'long') but the argument has type 'uint64_t' (aka 'unsigned long long') [-Wformat]

--- bigWig/src/jkweb/lib/itsa.c
+++ bigWig/src/jkweb/lib/itsa.c
@@ -68,7 +68,7 @@ if (h.majorVersion > ITSA_MAJOR_VERSION)
 	     "%s is version %d.",  fileName, ITSA_MAJOR_VERSION, fileName, h.majorVersion);
 
 struct itsa *itsa;
-verbose(2, "itsa file %s size %"PRIdMAX"\n", fileName, h.size);
+verbose(2, "itsa file %s size %"PRIu64"\n", fileName, h.size);
 
 /* Get a pointer to data in memory, via memory map, or allocation and read. */
 struct itsaFileHeader *header ;
